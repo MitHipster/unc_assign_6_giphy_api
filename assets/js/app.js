d0c84f250c5008c8e7b8988427c04f5fdcb9de93
Tim Acker <tjacker31@gmail.com>
2017-06-23 16:03:31 -0400

    AJAX Request
    Built AJAX function to request movie results from the GIPHY API.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,49 @@
 /*jslint esversion: 6, browser: true*/
 /*global window, console, $, jQuery, alert*/
 
+let search = 'http://api.giphy.com/v1/gifs/search?';
+const limit = 10;
+const rating = 'pg-13';
+const key = 'df241e37d4254980952f95c9742d1247';
+
+let movies = [
+  "The Sting",
+  "Gran Torino",
+  "Braveheart",
+  "Full Metal Jacket",
+  "The Green Mile",
+  "Raging Bull",
+  "The Departed",
+  "Lawrence of Arabia",
+  "The Shining",
+  "Terminator 2",
+  "Apocalypse Now",
+  "Dr. StrangeLove",
+  "Forrest Gump",
+  "The Usual Suspects",
+  "The Matrix",
+  "Raiders of the Lost Ark",
+  "Goodfellas",
+  "Fight Club",
+  "The Dark Knight",
+  "Inception",
+  "The Good, the Bad and the Ugly",
+  "The Godfather",
+  "The Shawshank Redemption"
+];
+
+
+search += $.param({
+  q: 'the matrix',
+  api_key: key,
+  limit: limit,
+  rating: rating
+});
+
+console.log(search);
+$.ajax({
+  url: search,
+  method: 'GET'
+}).done(function (results) {
+  console.log(results);
+});
